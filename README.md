486e47d5d972948103429222717a8160332940f9
Marc Sakalauskas <Marc@Marcs-iMac.local>
2019-06-19 06:48:57 -0400

    Updates

--- README.md
+++
@@ -1,7 +0,0 @@
-## Matching Game
-
-Memory card matching game built with React.
-- The objective of the Matching Game is for the player to turn over pairs of matching cards across eight successive turns. 
-- In a turn if the player selects two cards whose symbols match those cards, along with those successfully matched in previous turns, will remain up. However, if the player chooses two cards with different symbols they will both be flipped over, obscuring their symbols.
-- The game ends when all eight pairs of matching cards have been revealed. 
-- A 'New Game' Button gives the player the means to reset the game board.
---
+++ precache-manifest.d2b6f40fe7f099375e25c671f490fc48.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dec22b90b23a82fa2e63ef02bd89c5e6",
+    "url": "/matching-game/index.html"
+  },
+  {
+    "revision": "d60bf391e2912d5c6748",
+    "url": "/matching-game/static/css/main.02eb58de.chunk.css"
+  },
+  {
+    "revision": "add8163df1c2630ccff5",
+    "url": "/matching-game/static/js/2.54e2c2fb.chunk.js"
+  },
+  {
+    "revision": "d60bf391e2912d5c6748",
+    "url": "/matching-game/static/js/main.c6039c4a.chunk.js"
+  },
+  {
+    "revision": "e43d7fc81bcf6d217bd3",
+    "url": "/matching-game/static/js/runtime~main.c314e423.js"
+  },
+  {
+    "revision": "6b48e011cb3a7f96d087d1b9a2d94506",
+    "url": "/matching-game/static/media/cat-solid.6b48e011.svg"
+  },
+  {
+    "revision": "14678af6b6dd77ef75cc224dd9886db9",
+    "url": "/matching-game/static/media/crow-solid.14678af6.svg"
+  },
+  {
+    "revision": "4028858f5b05da2d670644e229c1a26e",
+    "url": "/matching-game/static/media/dog-solid.4028858f.svg"
+  },
+  {
+    "revision": "bcc2d9f4cf8dc321e8e3100250fbe25e",
+    "url": "/matching-game/static/media/dragon-solid.bcc2d9f4.svg"
+  },
+  {
+    "revision": "e1742c36db0bfb60858c009477aeb805",
+    "url": "/matching-game/static/media/fish-solid.e1742c36.svg"
+  },
+  {
+    "revision": "355024ddfe7c2d7d9d665a4f08af66b1",
+    "url": "/matching-game/static/media/frog-solid.355024dd.svg"
+  },
+  {
+    "revision": "c03b8464e3fdb051a4b81bf462627662",
+    "url": "/matching-game/static/media/hippo-solid.c03b8464.svg"
+  },
+  {
+    "revision": "a45e62308eedf4dd3c611ab64f6d48cc",
+    "url": "/matching-game/static/media/spider-solid.a45e6230.svg"
+  }
+]);
